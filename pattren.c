118fa3268efae82b42390a8660cd85554b87862f
dhanshbandari <dhanushsn17@gmail.com>
2026-03-26 23:37:11 +0530

    Pattern

--- pattren.c
+++ pattren.c
@@ -2,13 +2,11 @@
 
 int main(){
     int i ,j;
-     for (i =1;i<5+1;i++){  // Outer Loop for rows
-        for (j=1;j<i+(i-1)+1;j++){ // Inner Loop for columns
+     for (i =1;i<10+1;i++){    // Outer Loop for rows
+        for (j=1;j<i*(i-1)+1;j++){   // Inner Loop for columns
             printf("*" );
         }
         printf("\n");
      }
+        return 0;
 }
-
-
-
