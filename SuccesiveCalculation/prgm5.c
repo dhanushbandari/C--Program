ee46e5dd26e2722a42d24be45b4fa685719f4b3c
dhanshbandari <dhanushsn17@gmail.com>
2026-06-05 21:10:16 +0530

    Simple Update

--- SuccesiveCalculation/prgm5.c
+++ SuccesiveCalculation/prgm5.c
@@ -2,10 +2,10 @@
 
 #include <stdio.h>
 int main(){
-    int n=3,ans=0;
+    int n=2,ans=0;
     for(int i=2;i<n*2+1;i=i+2){ // for adding the odd number just give (i=0;i<=n*2-1;i=i+1);
         ans=ans+i;
     }
-    printf("Sum of odd numbers: %d", ans);
+    printf("Sum of even numbers: %d", ans);
 }
-
+ 
