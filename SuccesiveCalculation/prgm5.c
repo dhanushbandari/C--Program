50168a192f81f363429940ba6da4c65a2bd28c08
dhanshbandari <dhanushsn17@gmail.com>
2026-06-05 14:46:52 +0530

    simple Update

--- SuccesiveCalculation/prgm5.c
+++ SuccesiveCalculation/prgm5.c
@@ -1,21 +1,20 @@
-// #include <stdio.h>
-// int main(){
-//     int n=3,ans=0;
-//     for(int i=1;i<n*2-1;i=i+1){
-//         ans=ans+i;
-//     }
-//     printf("Sum of odd numbers: %d", ans);
-// }
-
 #include <stdio.h>
-
-int main() {
-    int n = 99;
-
-    if ((n / 2) * 2 == n)
-        printf("Even");
-    else
-        printf("Odd");
-
-    return 0;
+int main(){
+    int n=3,ans=0;
+    for(int i=1;i<n*2-1;i=i+1){
+        ans=ans+i;
+    }
+    printf("Sum of odd numbers: %d", ans);
 }
+
+// #include <stdio.h>
+
+// int main() {
+//     int n = 99;
+
+//     if ((n / 2) * 2 == n)
+//         printf("Even");
+//     else
+//         printf("Odd");
+//     return 0;
+// }
