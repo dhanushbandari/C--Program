cfb577eb20fd1ad9a24f16d79cf634b919e74806
dhanshbandari <dhanushsn17@gmail.com>
2026-06-05 20:38:50 +0530

    New Program

--- SuccesiveCalculation/prgm5.c
+++ SuccesiveCalculation/prgm5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int n=3,ans=0;
-    for(int i=1;i<n*2-1;i=i+1){
+    for(int i=2;i<n*2+1;i=i+2){
         ans=ans+i;
     }
     printf("Sum of odd numbers: %d", ans);
